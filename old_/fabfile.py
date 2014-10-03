895c7491cd52c222114298d4a5ffec0c25cde18f
epheo <root@epheo.eu>
2014-10-03 15:30:06 +0200

    add create & start

---
+++ old_/fabfile.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python2.7
+
+from fabric.api import *
+from fabric.state import env
+import docker
+import os                                                                 
+
+
+def build():
+    tags = ['epheo/openstack-base', 'epheo/os-mysql', 'epheo/os-rabbitmq', 'epheo/os-keystone', 'epheo/os-glance', 'epheo/os-nova', 'epheo/os-horizon']
+
+
+    docker_api.build(path=path, tag=tag)
+
+def install(host_name):
+
+    host_ip             = '172.17.42.1'
+    keystone_pass       = 'password'
+    glance_pass         = 'password'
+    nova_pass           = 'password'
+    admin_token         = 'password'
+    admin_password      = admin_token
+    keystone_host       = host_name
+    rabbitmq_host       = host_name
+    rabbitmq_password   = 'password'
+    mysql_pass          = 'password'
+    mysql_host          = host_name
+    mysql_user          = 'admin'
+
+    run("sudo docker stop  $(sudo docker ps | grep -v 'CONTAINER'  |gawk '{print $1}') ")
+
+    run("sudo docker run -d -p 3306:3306 -e MYSQL_PASS=%s epheo/os-mysql" % (mysql_pass))
+    run("sudo docker run -d -p  5672:5672 -p 15672:15672 -e RABBITMQ_PASSWORD=%s -v /var/log/openstack/rabbitmq:/var/log/supervisor epheo/os-rabbitmq" % (rabbitmq_password))
+    run("sleep 10")
+    run("sudo docker run -d -p 35357:35357 -p 5000:5000 -h='keystone' -e HOST_NAME=%s -e MYSQL_DB=%s -e MYSQL_USER=%s -e MYSQL_PASSWORD=%s -e ADMIN_TOKEN=%s -e KEYSTONE_DBPASS=%s -v /var/log/openstack/keystone:/var/log/supervisor epheo/os-keystone" % (host_name,mysql_host,mysql_user,mysql_pass,admin_token,mysql_pass))
+    run("sleep 5")
+    run("sudo docker run -d -p 9292:9292 -h='glance' -e HOST_NAME=%s -e MYSQL_DB=%s -e MYSQL_USER=%s -e MYSQL_PASSWORD=%s -e RABBITMQ_HOST=%s -e RABBITMQ_PASSWORD=%s -e GLANCE_DBPASS=%s -v /var/log/openstack/glance:/var/log/supervisor epheo/os-glance" % (host_name,mysql_host,mysql_user,mysql_pass,rabbitmq_host,rabbitmq_password,mysql_pass))
+    run("sleep 5")
+    run("sudo docker run -d -p 8774:8774 -p 8775:8775 -h='nova' -e HOST_NAME=%s -e HOST_IP=%s -e MYSQL_DB=%s -e MYSQL_USER=%s -e MYSQL_PASSWORD=%s -e RABBITMQ_HOST=%s -e RABBITMQ_PASSWORD=%s -e NOVA_DBPASS=%s -e ADMIN_PASS=%s --privileged -v /var/log/openstack/nova:/var/log/supervisor epheo/os-nova" % (host_name,mysql_host,mysql_user,mysql_pass,rabbitmq_host,rabbitmq_password,mysql_pass,admin_password))
+    run("sleep 5")
+    run("sudo docker run -d -p 80:80 -p 11211:11211 -h='horizon' -v /var/log/openstack/horizon:/var/log/supervisor -v /var/log/openstack/apache2:/var/log/apache2 -e HOST_NAME=%s epheo/os-horizon" % (host_name))
+
+
+def setup(host_name):
+    run("%s/create_default_user.sh" % path)
+    run("%s/keystone/create_user.sh keystone %s" % (path, host_name))
+    run("%s/keystone/register_service.sh keystone %s" % (path, host_name))
+    run("%s/glance/create_user.sh glance %s" % (path, host_name))
+    run("%s/glance/register_service.sh glance %s" % (path,host_name))
+    run("%s/nova/create_user.sh nova %s" % (path, host_name))
+    run("%s/nova/register_service.sh nova %s" % (path, host_name))
+
+def test(host_name):
+    with cd('test'):
+        run("%s/keystone_test.sh %s" % (path, host_name))
+        run("%s/glance_test.sh %s" % (path, host_name))
+
+class build(path, tag):
+
+
+def docker_deploy():
+    
+    docker_api.pull('epheo/os-nova', tag='latest')
+    osnova = docker_api.create_container('epheo/os-nova', 
+                                            volumes='/var/log/openstack/nova:/var/log/supervisor',
+                                            name='os-nova')
+    docker_api.start(es, port_bindings={8774: ('0.0.0.0', 8774), 8775: ('0.0.0.0', 8775)})
+
+if __name__ == '__main__':
+    path = os.getcwd()
+
+    
+
+    docker_api = docker.Client(base_url='unix://var/run/docker.sock',
+                           version='1.12',
+                           timeout=10)
+    host_name = 'node1'
+    hosts = ['localhost']
+    build(path, tag)
+    install()
--- py-devstack.py
+++ py-devstack.py
@@ -1,80 +1,185 @@
-#!/usr/bin/env python2.7
-
-from fabric.api import *
-from fabric.state import env
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#from fabric.api import *
 import docker
-import os                                                                 
+import os
+import socket
 
-
-def build():
-    tags = ['epheo/openstack-base', 'epheo/os-mysql', 'epheo/os-rabbitmq', 'epheo/os-keystone', 'epheo/os-glance', 'epheo/os-nova', 'epheo/os-horizon']
-
-
-    docker_api.build(path=path, tag=tag)
-
-def install(host_name):
-
-    host_ip             = '172.17.42.1'
+class Model(object):
     keystone_pass       = 'password'
     glance_pass         = 'password'
     nova_pass           = 'password'
     admin_token         = 'password'
+    rabbitmq_password   = 'password'
+    mysql_pass          = 'password'
+    mysql_user          = 'admin'
+    user                = 'py-devstack'
+
+    path                = os.getcwd()
+    host_ip             = [(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
+    host_name           = host_ip
     admin_password      = admin_token
     keystone_host       = host_name
     rabbitmq_host       = host_name
-    rabbitmq_password   = 'password'
-    mysql_pass          = 'password'
     mysql_host          = host_name
-    mysql_user          = 'admin'
 
-    run("sudo docker stop  $(sudo docker ps | grep -v 'CONTAINER'  |gawk '{print $1}') ")
+    services = {
+        'base': {
+            'tag': '%s/osbase' % (user), 
+            'path': '%s/base/' % (path),
+            },
 
-    run("sudo docker run -d -p 3306:3306 -e MYSQL_PASS=%s epheo/os-mysql" % (mysql_pass))
-    run("sudo docker run -d -p  5672:5672 -p 15672:15672 -e RABBITMQ_PASSWORD=%s -v /var/log/openstack/rabbitmq:/var/log/supervisor epheo/os-rabbitmq" % (rabbitmq_password))
-    run("sleep 10")
-    run("sudo docker run -d -p 35357:35357 -p 5000:5000 -h='keystone' -e HOST_NAME=%s -e MYSQL_DB=%s -e MYSQL_USER=%s -e MYSQL_PASSWORD=%s -e ADMIN_TOKEN=%s -e KEYSTONE_DBPASS=%s -v /var/log/openstack/keystone:/var/log/supervisor epheo/os-keystone" % (host_name,mysql_host,mysql_user,mysql_pass,admin_token,mysql_pass))
-    run("sleep 5")
-    run("sudo docker run -d -p 9292:9292 -h='glance' -e HOST_NAME=%s -e MYSQL_DB=%s -e MYSQL_USER=%s -e MYSQL_PASSWORD=%s -e RABBITMQ_HOST=%s -e RABBITMQ_PASSWORD=%s -e GLANCE_DBPASS=%s -v /var/log/openstack/glance:/var/log/supervisor epheo/os-glance" % (host_name,mysql_host,mysql_user,mysql_pass,rabbitmq_host,rabbitmq_password,mysql_pass))
-    run("sleep 5")
-    run("sudo docker run -d -p 8774:8774 -p 8775:8775 -h='nova' -e HOST_NAME=%s -e HOST_IP=%s -e MYSQL_DB=%s -e MYSQL_USER=%s -e MYSQL_PASSWORD=%s -e RABBITMQ_HOST=%s -e RABBITMQ_PASSWORD=%s -e NOVA_DBPASS=%s -e ADMIN_PASS=%s --privileged -v /var/log/openstack/nova:/var/log/supervisor epheo/os-nova" % (host_name,mysql_host,mysql_user,mysql_pass,rabbitmq_host,rabbitmq_password,mysql_pass,admin_password))
-    run("sleep 5")
-    run("sudo docker run -d -p 80:80 -p 11211:11211 -h='horizon' -v /var/log/openstack/horizon:/var/log/supervisor -v /var/log/openstack/apache2:/var/log/apache2 -e HOST_NAME=%s epheo/os-horizon" % (host_name))
+        'mysql': {
+            'tag': '%s/osmysql' % (user), 
+            'path': '%s/mysql/' % (path),
+            'ports': {3306: ('0.0.0.0', 3306)},
+            'confs': {'MYSQL_PASS=%s' % (mysql_pass)},
+            'volumes': {'/var/log/openstack/rabbitmq:/var/log/supervisor'}
+            },
+
+        'rabbitmq': {
+            'tag': '%s/osrabbitmq' % (user), 
+            'path': '%s/rabbitmq/' % (path),
+            'ports': {5672: ('0.0.0.0', 5672), 15672: ('0.0.0.0', 15672)},
+            'confs': {'RABBITMQ_PASSWORD=%s' % (rabbitmq_password) },
+            'volumes': {'/var/log/openstack/rabbitmq:/var/log/supervisor'}
+            },
+
+        'glance': {
+            'tag': '%s/osglance' % (user), 
+            'path': '%s/glance/' % (path),
+            'ports': {9292: ('0.0.0.0', 9292)},
+            'confs': {'HOST_NAME=%s' % (host_name), 
+                      'MYSQL_DB=%s' % (mysql_host), 
+                      'MYSQL_USER=%s' % (mysql_user), 
+                      'MYSQL_PASSWORD=%s' % (mysql_pass), 
+                      'RABBITMQ_HOST=%s' % (rabbitmq_host), 
+                      'RABBITMQ_PASSWORD=%s' % (rabbitmq_password), 
+                      'GLANCE_DBPASS=%s' % (glance_pass)
+                     },
+            'volumes': {'/var/log/openstack/glance:/var/log/supervisor'}
+            },
+
+        'horizon': {
+            'tag': '%s/oshorizon' % (user), 
+            'path': '%s/horizon/' % (path),
+            'ports': {80: ('0.0.0.0', 80), 11211: ('0.0.0.0', 11211)},
+            'confs': {'HOST_NAME=%s' % (host_name) },
+            'volumes': {'/var/log/openstack/horizon:/var/log/supervisor', '/var/log/openstack/apache2:/var/log/apache2'}
+            },
+
+        'keystone': {
+            'tag': '%s/oskeystone' % (user), 
+            'path': '%s/keystone/' % (path),
+            'ports': {35357: ('0.0.0.0', 35357), 5000: ('0.0.0.0', 5000)},
+            'confs': {'HOST_NAME=%s' % (host_name), 
+                      'MYSQL_DB=%s' % (mysql_host), 
+                      'MYSQL_USER=%s' % (mysql_user), 
+                      'MYSQL_PASSWORD=%s' % (mysql_pass), 
+                      'ADMIN_TOKEN=%s' % (admin_token), 
+                      'KEYSTONE_DBPASS=%s' % (keystone_pass)
+                     },
+            'volumes': {'/var/log/openstack/keystone:/var/log/supervisor'}
+            },
+
+        'nova': {
+            'tag': '%s/osnova' % (user), 
+            'path': '%s/nova/' % (path),
+            'ports': {8774: ('0.0.0.0', 8774), 8775: ('0.0.0.0', 8775)},
+            'confs': {'HOST_NAME=%s' % (host_name), 
+                      'HOST_IP=%s' % (host_ip), 
+                      'MYSQL_DB=%s' % (mysql_host), 
+                      'MYSQL_USER=%s' % (mysql_user), 
+                      'MYSQL_PASSWORD=%s' % (mysql_pass), 
+                      'RABBITMQ_HOST=%s' % (rabbitmq_host), 
+                      'RABBITMQ_PASSWORD=%s' % (rabbitmq_password), 
+                      'NOVA_DBPASS=%s' % (nova_pass), 
+                      'ADMIN_PASS=%s' % (admin_password)
+                     },
+            'volumes': {'/var/log/openstack/nova:/var/log/supervisor'},
+            'privileged': True
+            },
+
+        'novacompute': {
+            'tag': '%s/osnovacompute' % (user), 
+            'path': '%s/novacompute/' % (path),
+            'confs': {''},
+            'volumes': {'/var/log/openstack/novacompute:/var/log/supervisor'}
+            },
+
+    }
+
+class View(object):
+
+    def service_list(self, service_list):
+        print('service LIST:')
+        for service in service_list:
+            print(service)
+        print('')
+
+    def service_information(self, action, name, *argv):
+        print('%s service %s with aruments:' % (action, name) )
+        for arg in argv:
+            print arg
+        print('')
 
 
-def setup(host_name):
-    run("%s/create_default_user.sh" % path)
-    run("%s/keystone/create_user.sh keystone %s" % (path, host_name))
-    run("%s/keystone/register_service.sh keystone %s" % (path, host_name))
-    run("%s/glance/create_user.sh glance %s" % (path, host_name))
-    run("%s/glance/register_service.sh glance %s" % (path,host_name))
-    run("%s/nova/create_user.sh nova %s" % (path, host_name))
-    run("%s/nova/register_service.sh nova %s" % (path, host_name))
-
-def test(host_name):
-    with cd('test'):
-        run("%s/keystone_test.sh %s" % (path, host_name))
-        run("%s/glance_test.sh %s" % (path, host_name))
-
-class build(path, tag):
+    def service_not_found(self, name):
+        print('That service "%s" does not exist' % name)
 
 
-def docker_deploy():
-    
-    docker_api.pull('epheo/os-nova', tag='latest')
-    osnova = docker_api.create_container('epheo/os-nova', 
-                                            volumes='/var/log/openstack/nova:/var/log/supervisor',
-                                            name='os-nova')
-    docker_api.start(es, port_bindings={8774: ('0.0.0.0', 8774), 8775: ('0.0.0.0', 8775)})
+class Controller(object):
+
+    def __init__(self):
+        docker_api = docker.Client(base_url='unix://var/run/docker.sock', version='1.12', timeout=10)
+        self.model = Model()
+        self.view = View()
+
+    def exec_service_list(self, action):
+        service_list = self.model.services.keys()
+        self.view.service_list(service_list)
+
+        for service in service_list:
+            service_info = self.model.services.get(service, None)
+            if service_info is not None:
+
+                name            = service.title()
+                tag             = service_info.get('tag', 0)
+                path            = service_info.get('path', 0)
+                port_bindings   = service_info.get('ports', 0)
+                confs           = service_info.get('confs', 0)
+                volumes         = service_info.get('volumes', 0)
+
+                controller.build_service_container(name, tag, path)
+                controller.build_service_container(name, tag, path)
+                controller.create_service_container(name, tag, volumes)
+                controller.start_service_container(name, port_bindings, confs)
+
+            else:
+                self.view.service_not_found(name)
+
+    def build_service_container(self, name, tag, path):
+        action='building'
+        self.view.service_information(action, name, tag, path)
+        for line in docker_api.build(path, tag):
+            print(line)
+
+
+    def create_service_container(self, name, tag, volumes):
+        action='creating'
+        self.view.service_information(action, name, tag, volumes)
+        id_image = docker_api.create_container(tag, volumes, name)
+
+
+    def start_service_container(self, name, port_bindings, confs):
+        action='starting'
+        self.view.service_information(action, name, port_bindings, confs)
+        id_container = docker_api.start(name, port_bindings)
+
+
 
 if __name__ == '__main__':
-    path = os.getcwd()
+    controller = Controller()
 
-    
-
-    docker_api = docker.Client(base_url='unix://var/run/docker.sock',
-                           version='1.12',
-                           timeout=10)
-    host_name = 'node1'
-    hosts = ['localhost']
-    build(path, tag)
-    install()
+    action='build'
+    controller.exec_service_list(action)
