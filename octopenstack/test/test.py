7655822494f28ea891b63801f80418787cf62424
epheo <root@epheo.eu>
2014-10-21 17:13:47 +0200

    Packaging ok, start unit test but not yet

--- octopenstack/test/test.py
+++ octopenstack/test/test.py
@@ -17,5 +17,13 @@
 
 import unittest
 
+class FooTests(unittest.TestCase):
+
+    def testFoo(self):
+        self.failUnless(True)
+
+def main():
+    unittest.main()
+
 if __name__ == '__main__':
-    exit(0)
+    main()
