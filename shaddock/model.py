cec4018d0099e219dbd49b83946941ebaf2352ba
weezheel <skylostft@laposte.net>
2015-05-14 23:48:48 +0200

    This commit replace the string infrastructure.yml by the template name supplied as an argument

--- shaddock/model.py
+++ shaddock/model.py
@@ -52,6 +52,7 @@ class ContainerConfig(object):
             self.__construct(name)
 
     def __construct(self, name):
+        template_file = self.app_args.template_file
         services_list = get_services_list(self.app_args)
         try:
             service = [svc for svc in services_list if
@@ -59,24 +60,24 @@ class ContainerConfig(object):
             if len(service) > 1:
                 raise TemplateFileError(
                     "There is more than one definition matching"
-                    " 'name: {}' in your infrastrucure.yml".format(name))
+                    " 'name: {}' in your {}".format(name, template_file))
             service = service[0]
         except IndexError:
             raise TemplateFileError(
                 "There is no container definition containing"
-                " 'name: {}' in your infrastrucure.yml".format(name))
+                " 'name: {}' in your {}".format(name, template_file))
         except KeyError:
             raise TemplateFileError(
                 "At least one container definition in your"
-                " infrastructure.yml is missing the name property")
+                " {} is missing the name property".format(template_file))
 
         self.name = name
         try:
             self.tag = service['image']
         except KeyError:
             raise TemplateFileError(
-                "Container definition of: '{}' in your infrastructure.yml is"
-                " missing the image property".format(name))
+                "Container definition of: '{}' in your {} is"
+                " missing the image property".format(name, template_file))
 
         self.privileged = service.get('privileged')
         self.env = service.get('env')
@@ -107,8 +108,8 @@ class ContainerConfig(object):
             except KeyError:
                 raise TemplateFileError(
                     "A container's volume definition in your"
-                    " infrastructure.yml is missing the mount or host_dir"
-                    " property")
+                    " {} is missing the mount or host_dir"
+                    " property".format(template_file))
 
         #  The returned object will look like:
         #      'name': 'nova',
