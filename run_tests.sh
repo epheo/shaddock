65941a74374d9b42b9a33e2159fd040e7ed28991
epheo <thibaut@hpe.com>
2016-11-24 03:13:15 +0000

    Fix tests

--- run_tests.sh
+++ run_tests.sh
@@ -4,7 +4,7 @@ shaddock -f tests/model/010-img-tests.yml build
 
 for test_scenario in 010-img-tests.yml \
                      130-volume-tests.yml \
-                     120-cmd-tests.yml \
+                     120-include-tests.yml \
                      200-hosts-tests.yml \
                      300-network-tests.yml \
                      400-jinja-tests.yml \
