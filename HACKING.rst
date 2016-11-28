ee395da7f2150a120f571051d625503e4f0d0f76
epheo <thibaut@hpe.com>
2016-11-28 21:22:46 +0000

    Documentation and tests

--- HACKING.rst
+++ HACKING.rst
@@ -3,8 +3,8 @@ shaddock style commandments
 
 Step 0
 ~~~~~~
-Step 1: Read pep8
-Step 2: Read pep8 again
-Step 3: Read on
+* Step 1: Read pep8
+* Step 2: Read pep8 again
+* Step 3: Read on
 
 Read the OpenStack Style Commandments http://docs.openstack.org/developer/hacking/
--- run_tests.sh
+++ run_tests.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
+shaddock -f tests/model/shaddock.yml rm
 shaddock -f tests/model/010-img-tests.yml build
 shaddock -f tests/model/shaddock.yml ps
-
-for test_scenario in 010-img-tests.yml \
-                     130-volume-tests.yml \
-                     120-include-tests.yml \
-                     200-hosts-tests.yml \
-                     300-network-tests.yml \
-                     400-jinja-tests.yml \
-                     500-scheduler-tests.yml
-do
-  echo ""
-  echo "# Running test scenario $test_scenario"
-  echo "# ------------------------------------"
-  # $1=--debug
-  # $2=-vvv"
-  shaddock -f tests/model/$test_scenario $1 $2 rm > /dev/null
-  shaddock -f tests/model/$test_scenario $1 $2 start
-  shaddock -f tests/model/$test_scenario $1 $2 rm > /dev/null
-  # shaddock -f tests/model/$test_scenario $opts logs |tail -n600
-done
+shaddock -f tests/model/shaddock.yml start
+shaddock -f tests/model/shaddock.yml rm
