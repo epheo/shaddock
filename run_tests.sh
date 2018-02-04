b05eccd191519055600b3bcc73e8cdede47a443f
Thibaut Lapierre <github@epheo.eu>
2018-02-04 19:38:05 +0000

    Fix tests multi-hosts

--- run_tests.sh
+++ run_tests.sh
@@ -5,4 +5,4 @@ shdk -f tests/model/010-img-tests.yml build    --debug   # --verbose
 shdk -f tests/model/shaddock.yml ps            --debug   # --verbose   
 shdk -f tests/model/shaddock.yml start         --debug   # --verbose   
 shdk -f tests/model/shaddock.yml rm            --debug   # --verbose   
-shdk -f tests/model/600-load-tests.yaml ps            --debug   # --verbose   
+shdk -f tests/model/600-load-tests.yml ps      --debug   # --verbose   
